0186eaf66c09fbd3245e874f0243e0c9793b6467
Rayirth Soni <rayirth.soni@finbox.in>
2024-05-06 10:07:49 +0530

    added code for create folder function

--- WebScraping/scrape_paragraph.py
+++ WebScraping/scrape_paragraph.py
@@ -1,49 +1,87 @@
 import requests
 import bs4
-import pandas as pd
 import os
 import re
 
-# Folder Path where text will be written
-base_path = os.getcwd()
-text_folder_path = os.path.join(base_path, 'WebScraping', 'Paragraph Text')
-
+# path for directory on which will be working on 
+CURRENT_WORKING_DIRECTORY_PATH = os.getcwd()
+# path for webscarping folder
+WEBSCRAPING_FOLDER_PATH = os.path.join(CURRENT_WORKING_DIRECTORY_PATH, 'WebScraping')
 
 # url from which to fetch data
-url = 'https://en.wikipedia.org/wiki/India'
+WEBPAGE_URL = 'https://en.wikipedia.org/wiki/India'
 
 # Html Content for webpage
-html_content = requests.get(url)
+html_content = requests.get(WEBPAGE_URL)
 
 # soup object for html page
 soup = bs4.BeautifulSoup(html_content.text, 'lxml')
 
-
-def get_webpage():
+# Function to create folder
+def create_folder(folder_name):
+    
     try:
-        html_content = requests.get(url)
-        soup = bs4.BeautifulSoup(html_content.text, 'lxml')
-        return soup
-    except Exception as e:
-        print(e)
         
-def get_paragraph_text():
-    try:
-        entire_paragraph = ''
-        # html paragraphs to write in the file
-        paragraphs_html = soup.find_all('p')
-        paragraphs_html = paragraphs_html[:5]
-        for paragraph in paragraphs_html:
-            paragraph_text = paragraph.text
-            pattern = r'\[[^\]]*\]'
-            paragraph_text = re.sub(pattern, '', paragraph_text)
-            entire_paragraph += paragraph_text.strip()
-        return entire_paragraph
+        text_folder_path = os.path.join(CURRENT_WORKING_DIRECTORY_PATH,folder_name)
+        
+        if not os.path.exists(text_folder_path):
+            os.makedirs(text_folder_path)
+            print(f'Folder Created at : {text_folder_path}')
+            
+        else:
+            print(f'Folder Already exists at : {text_folder_path}')
     
     except Exception as e:
         print(e)
 
 
+
+def get_webpage():
+
+    try:
+        
+        html_content = requests.get(WEBPAGE_URL)
+        soup = bs4.BeautifulSoup(html_content.text, 'lxml')
+
+        return soup
+    
+    except Exception as e:
+        print(e)
+
+
+def get_paragraph_text():
+
+    try:
+        # text to add entire paragraph
+        entire_paragraph = ''
+
+        # html paragraphs to write in the file
+        paragraphs_html = soup.find_all('p')
+        paragraphs_html = paragraphs_html[:5]
+
+        for paragraph in paragraphs_html:
+            paragraph_text = paragraph.text
+            pattern = r'\[[^\]]*\]'
+            paragraph_text = re.sub(pattern, '', paragraph_text)
+            entire_paragraph += paragraph_text.strip()
+
+        return entire_paragraph
+
+    except Exception as e:
+        print(e)
+
+def write_content_to_file():
+    
+    create_folder
+    
+    try:
+        
+        pass
+    
+    except Exception as e:
+        print(e)
+    
+
+
 soup = get_webpage()
 entire_paragraph = get_paragraph_text()
-
